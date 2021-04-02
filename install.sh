ed0fb5b8aceb28976e7484847de35653c1c8c21d
Etxau24 <96jonetxauri@gmail.com>
2021-04-02 17:12:15 +0200

    install & laguntza lenguage

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ gre='\e[0;32m';
 yel='\e[1;33m';
 blu='\e[1;34m';
 whi='\e[0;37m';
+os_marked=0
 
 # Defining variables
 confile='conf'
@@ -18,12 +19,23 @@ echo
 echo -e "${blu}> Add your needed helps"
 for issue in $issues
 do
-    echo -e '${whi}Do you want to get info about ' + $issue + ' printed? (Y/n)'
-    read -sn 1 answere
-    
-    if [[ "$answere" == "y" || "$answere" == "" ]]
+    if [[ $(echo $issue | cut -c1-2) != "os" ]]
     then
-        sed -i -r 's/#$issue/$issue/g' $confile
+        echo -e "${whi}Do you want to get info about" $issue "printed? (Y/n)"
+        read -sn 1 answere
+
+        if [[ "$answere" == "y" || "$answere" == "" ]]
+        then
+            issu=$(echo $issue | cut -c2-)
+            sed -i 's,#'"$issu"','"$issu"',' conf
+        else
+            if [[ $(echo $issue | cut -c1) != "#" ]]
+            then
+                sed -i 's,'"$issue"',#'"$issue"',' conf        
+            fi
+        fi
+    else
+        os_marked=1
     fi
 done
 
@@ -31,23 +43,29 @@ done
 echo -e "${blu}> Obtaining OS..."
 os=$(cat /etc/os-release | grep -E '\bID='| tr "=" "\n" | grep -v ID)
 echo -e "${gre}> Looks like you are using $os."
-echo "os=$os" >> conf
+if [[ os_marked -eq 0 ]]
+then
+    echo "os=$os" >> conf
+fi
 
 # Set executer alias
 echo -e "${blu}> Creating alias for execution..."
+echo -e "${gre}> Looks like your are using" $SHELL "emulator"
+
 alias=$(echo "sh $(pwd)/laguntza.sh")
 if [[ "$SHELL" == *zsh* ]]
 then
     echo "alias laguntza=\"$alias\"" >> ~/.zshrc
-    echo "$(red)run 'source ~/.zshrc' to restart shell configuration."
+    echo -e "${red}run 'source ~/.zshrc' to restart shell configuration."
 elif [[ "$SHELL" == *bash* ]]
 then
     echo "alias laguntza=\"$alias\"" >> ~/.bashrc
-    echo "$(red)run 'source ~/.bashrc' to restart shell configuration."
-elif [[ "$SHELL" == *fish* ]]
-then
-#    function laguntza; $alias; end; && funcsave laguntza
+    echo -e "${red}run 'source ~/.bashrc' to restart shell configuration."
+#elif [[ "$SHELL" == *fish* ]]
+#then
+#    echo function laguntza; $alias; end; && funcsave laguntza
 fi
 
 # Finished.
-echo -e "${gre}> Succesfully finished. Try 'laguntza' after restarting shell configuration."
+echo -e "${yel}> Succesfully finished. Try 'laguntza' after restarting shell configuration."
+exit 0
